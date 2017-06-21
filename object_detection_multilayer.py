915371b98fe6d10388cb775f3e86d2cfd6531b06
Dat Tran <datitran@gmail.com>
2017-06-21 10:05:42 +0200

    Improving speed by moving video capture to multithreading.

--- object_detection_multilayer.py
+++ object_detection_multilayer.py
@@ -1,10 +1,12 @@
-import time
-import os
 import cv2
+import multiprocessing
+import time
+
+import os
 import numpy as np
 import tensorflow as tf
 
-from utils import FPS, WebcamVideoStream
+from utils import FPS
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
@@ -54,10 +56,44 @@ def detect_objects(image_np, sess, detection_graph):
         category_index,
         use_normalized_coordinates=True,
         line_thickness=8)
+
     return image_np
 
 
-if __name__ == '__main__':
+def blend_non_transparent(face_img, overlay_img):
+    # Let's find a mask covering all the non-black (foreground) pixels
+    # NB: We need to do this on grayscale version of the image
+    gray_overlay = cv2.cvtColor(overlay_img, cv2.COLOR_BGR2GRAY)
+    overlay_mask = cv2.threshold(gray_overlay, 1, 255, cv2.THRESH_BINARY)[1]
+
+    # Let's shrink and blur it a little to make the transitions smoother...
+    overlay_mask = cv2.erode(overlay_mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
+    overlay_mask = cv2.blur(overlay_mask, (3, 3))
+
+    # And the inverse mask, that covers all the black (background) pixels
+    background_mask = 255 - overlay_mask
+
+    # Turn the masks into three channel, so we can use them as weights
+    overlay_mask = cv2.cvtColor(overlay_mask, cv2.COLOR_GRAY2BGR)
+    background_mask = cv2.cvtColor(background_mask, cv2.COLOR_GRAY2BGR)
+
+    # Create a masked out face image, and masked out overlay
+    # We convert the images to floating point in range 0.0 - 1.0
+    face_part = (face_img * (1 / 255.0)) * (background_mask * (1 / 255.0))
+    overlay_part = (overlay_img * (1 / 255.0)) * (overlay_mask * (1 / 255.0))
+
+    # And finally just add them together, and rescale it back to an 8bit integer image
+    return np.uint8(cv2.addWeighted(face_part, 255.0, overlay_part, 255.0, 0.0))
+
+
+def main_process(input, output):
+    while True:
+        time.sleep(0.5)
+        image = input.get()
+        output.put(image)
+
+
+def child_process(input, output):
     # Load a (frozen) Tensorflow model into memory.
     detection_graph = tf.Graph()
     with detection_graph.as_default():
@@ -69,34 +105,35 @@ if __name__ == '__main__':
 
         sess = tf.Session(graph=detection_graph)
 
-    video_capture = WebcamVideoStream(src=0).start()
-    fps = FPS().start()
+    while True:
+        image = input.get()
+        image2 = detect_objects(image, sess, detection_graph)
+        result = blend_non_transparent(image, image2)
+        output.put(result)
 
-    while fps._numFrames < 120:
-        frame = video_capture.read()
 
-        t = time.time()
+if __name__ == '__main__':
+    input = multiprocessing.Queue(5)
+    output = multiprocessing.Queue(5)
 
-        # cv2.imshow('Video', detect_objects(frame, sess, detection_graph))
+    main_process = multiprocessing.Process(target=main_process, args=(input, output))
+    main_process.daemon = True
+    child_process = multiprocessing.Process(target=child_process, args=(input, output))
+    child_process.daemon = False
 
-        # time.sleep(2)
+    main_process.start()
+    child_process.start()
 
-        detect_objects(frame, sess, detection_graph)
+    video_capture = cv2.VideoCapture(0)
+    video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+    video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
-        print(time.time() - t)
+    while True:
+        _, frame = video_capture.read()
 
-        # update the FPS counter
-        fps.update()
+        input.put(frame)
+
+        cv2.imshow('Video', output.get())
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-    # stop the timer and display FPS information
-    fps.stop()
-    print('[INFO] elasped time: {:.2f}'.format(fps.elapsed()))
-    print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
-
-    # When everything is done, release the capture
-    video_capture.stop()
-    cv2.destroyAllWindows()
-    sess.close()
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import cv2
 import datetime
+from threading import Thread
 
 
 class FPS:
@@ -31,3 +33,40 @@ class FPS:
     def fps(self):
         # compute the (approximate) frames per second
         return self._numFrames / self.elapsed()
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+        (self.grabbed, self.frame) = self.stream.read()
+
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
